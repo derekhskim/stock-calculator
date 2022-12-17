4979051b9e87ddd89d3c9e438e694c142db17e2e
[treasure3210] <treasure3210@gmail.com>
2022-12-17 13:47:22 -0700

    Created a method to calculate the average of given number of stocks and purchased prices and also create another method for calculating the total number of stocks just for the viewing purposes. Created a button using for loop so that user can press to create more fields to enter.
    The average and total amount of stocks will not be visible until "calculate" button has been pressed.
    Total number of stocks limited to 2 decimal points, due to possibilities of fractional buys
    Average amount limited to 2 decimal points for visibility
    
    Created FocusState for hiding the keypad when the user taps elsewhere from the textfield.

--- Stock Calculator/ContentView.swift
+++ Stock Calculator/ContentView.swift
@@ -1,42 +1,118 @@
-//
-//  ContentView.swift
-//  Stock Calculator
-//
-//  Created by Derek Kim on 2022/11/30.
-//
-
 import SwiftUI
-import Combine
 
-struct ContentView: View {
+struct StockAverageCalculator: View {
     
-    @State var purchasedPrice = ""
-    @State var purchasedAmount = ""
+    enum Field: Hashable {
+            case myField
+        }
+    
+    // State variables to store the input values
+    @State private var stockAmount = ""
+    @State private var purchasePrice = ""
+    @State private var stockFields: [[String]] = [["", ""]]
+    
+    // State variable to toggle the visibility of the average section
+    @State private var showAverage = false
+    @FocusState private var focusedField: Field?
     
     var body: some View {
-            VStack {
-                Text("Stock Average Caluclator")
-                TextField(
-                  "Shares purchased price",
-                  text: $purchasedPrice
-                )
-                TextField(
-                    "Shares Bought", text: $purchasedAmount)
-                .keyboardType(.numberPad)
-                .onReceive(Just(purchasedAmount)) { newValue in
-                    let filtered = newValue.filter {
-                        "0123456789".contains($0) }
-                    if filtered != newValue {
-                        self.purchasedAmount = filtered
+        NavigationView {
+            Form {
+                Section(header: Text("Add Stock")) {
+                    // TextField to input the amount of stocks purchased
+                    TextField("Amount of stocks purchased", text: $stockAmount)
+                        .focused($focusedField, equals: .myField)
+                        .keyboardType(.decimalPad)
+                    // TextField to input the purchase price
+                    TextField("Purchase price", text: $purchasePrice)
+                        .focused($focusedField, equals: .myField)
+                        .keyboardType(.decimalPad)
+                }
+                // For each set of fields in the stockFields array, create a section with two TextFields
+                ForEach(0..<stockFields.count, id: \.self) { index in
+                    Section {
+                        TextField("Amount of stocks purchased", text: self.$stockFields[index][0])
+                            .keyboardType(.decimalPad)
+                        TextField("Purchase price", text: self.$stockFields[index][1])
+                            .keyboardType(.decimalPad)
+                    }
+                }
+                
+                // Button to add another set of fields
+                Button(action: {
+                    self.stockFields.append(["", ""])
+                    focusedField = nil
+                }) {
+                    Text("Add another stock")
+                }
+                
+                // Add a "Calculate" button to show the average section
+                Section {
+                    Button(action: {
+                        self.showAverage = true
+                        focusedField = nil
+                    }) {
+                        Text("Calculate")
+                    }
+                }
+                // Only show the average section if the showAverage state variable is true
+                if showAverage {
+                    Section(header: Text("Average Result")) {
+                        // Display the total amount of stocks purchased & average stock price
+                        Text("Total Stocks Purcahsed: \(calculateTotalAmount(), specifier: "%.2f")")
+                        Text("Average Price: \(calculateAverage(), specifier: "%.2f")")
                     }
                 }
             }
-            .padding()
+            .navigationBarTitle("Stock Average Calculator")
         }
+    }
+    
+    // Calculate the total amount of stocks
+    func calculateTotalAmount() -> Double {
+        var totalAmount = 0.0
+        
+        // Add the values from the initial fields
+        if let stockAmount = Double(stockAmount) {
+            totalAmount += stockAmount
+        }
+        
+        // Add the values from the additional fields
+        for fields in stockFields {
+            if let stockAmount = Double(fields[0]) {
+                totalAmount += stockAmount
+            }
+        }
+        
+        return totalAmount
+    }
+    
+    // Calculate the average stock price
+    func calculateAverage() -> Double {
+        var totalAmount = 0.0
+        var totalPrice = 0.0
+        
+        // Add the values from the initial fields
+        if let stockAmount = Double(stockAmount), let purchasePrice = Double(purchasePrice) {
+            totalAmount += stockAmount
+            totalPrice += stockAmount * purchasePrice
+        }
+        
+        // Add the values from the additional fields
+        for fields in stockFields {
+            if let stockAmount = Double(fields[0]), let purchasePrice = Double(fields[1]) {
+                totalAmount += stockAmount
+                totalPrice += stockAmount * purchasePrice
+            }
+        }
+        
+        // Calculate and return the average
+        return totalPrice / totalAmount
+    }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct StockAverageCalculator_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        StockAverageCalculator()
     }
 }
